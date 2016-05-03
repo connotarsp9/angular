connotarsp9/angular

commit 3978b16231f8c9da53c8364ab3d574c900b7a3d2

fix(compiler): properly implement pure pipes and change pipe syntax
Pure pipes as well as arrays and maps are
implemented via proxy functions. This is
faster than the previous implementation
and also generates less code.

BREAKING CHANGE:
- pipes now take a variable number of arguments, and not an array that contains all arguments.

modules/angular2/src/compiler/view_compiler/compile_pipe.ts
@@ -1,0 +1,76 @@
+import {isBlank, isPresent} from 'angular2/src/facade/lang';
+import {BaseException} from 'angular2/src/facade/exceptions';
+import * as o from '../output/output_ast';
+import {CompileView} from './compile_view';
+import {CompilePipeMetadata} from '../compile_metadata';
+import {Identifiers, identifierToken} from '../identifiers';
+import {injectFromViewParentInjector, createPureProxy, getPropertyInView} from './util';
+
+class _PurePipeProxy {
+  constructor(public instance: o.ReadPropExpr, public argCount: number) {}
+}
+
+export class CompilePipe {
+  meta: CompilePipeMetadata;
+  instance: o.ReadPropExpr;
+  private _purePipeProxies: _PurePipeProxy[] = [];
+
+  constructor(public view: CompileView, name: string) {
+    this.meta = _findPipeMeta(view, name);
+    this.instance = o.THIS_EXPR.prop(`_pipe_${name}_${view.pipeCount++}`);
+  }
+
+  get pure(): boolean { return this.meta.pure; }
+
+  create(): void {
+    var deps = this.meta.type.diDeps.map((diDep) => {
+      if (diDep.token.equalsTo(identifierToken(Identifiers.ChangeDetectorRef))) {
+        return o.THIS_EXPR.prop('ref');
+      }
+      return injectFromViewParentInjector(diDep.token, false);
+    });
+    this.view.fields.push(new o.ClassField(this.instance.name, o.importType(this.meta.type),
+                                           [o.StmtModifier.Private]));
+    this.view.createMethod.resetDebugInfo(null, null);
+    this.view.createMethod.addStmt(o.THIS_EXPR.prop(this.instance.name)
+                                       .set(o.importExpr(this.meta.type).instantiate(deps))
+                                       .toStmt());
+    this._purePipeProxies.forEach((purePipeProxy) => {
+      createPureProxy(
+          this.instance.prop('transform').callMethod(o.BuiltinMethod.bind, [this.instance]),
+          purePipeProxy.argCount, purePipeProxy.instance, this.view);
+    });
+  }
+
+  call(callingView: CompileView, args: o.Expression[]): o.Expression {
+    if (this.meta.pure) {
+      var purePipeProxy = new _PurePipeProxy(
+          o.THIS_EXPR.prop(`${this.instance.name}_${this._purePipeProxies.length}`), args.length);
+      this._purePipeProxies.push(purePipeProxy);
+      return getPropertyInView(
+                 o.importExpr(Identifiers.castByValue)
+                     .callFn([purePipeProxy.instance, this.instance.prop('transform')]),
+                 callingView, this.view)
+          .callFn(args);
+    } else {
+      return getPropertyInView(this.instance, callingView, this.view).callMethod('transform', args);
+    }
+  }
+}
+
+
+function _findPipeMeta(view: CompileView, name: string): CompilePipeMetadata {
+  var pipeMeta: CompilePipeMetadata = null;
+  for (var i = view.pipeMetas.length - 1; i >= 0; i--) {
+    var localPipeMeta = view.pipeMetas[i];
+    if (localPipeMeta.name == name) {
+      pipeMeta = localPipeMeta;
+      break;
+    }
+  }
+  if (isBlank(pipeMeta)) {
+    throw new BaseException(
+        `Illegal state: Could not find pipe ${name} although the parser should have detected this error!`);
+  }
+  return pipeMeta;
+}

modules/angular2/src/compiler/view_compiler/compile_view.ts
@@ -1,14 +1,13 @@
 import {isPresent, isBlank} from 'angular2/src/facade/lang';
-import {ListWrapper, StringMapWrapper} from 'angular2/src/facade/collection';
-import {BaseException} from 'angular2/src/facade/exceptions';
+import {ListWrapper, StringMapWrapper, MapWrapper} from 'angular2/src/facade/collection';
 
 import * as o from '../output/output_ast';
-import {Identifiers, identifierToken} from '../identifiers';
 import {EventHandlerVars} from './constants';
 import {CompileQuery, createQueryList, addQueryToTokenMap} from './compile_query';
 import {NameResolver} from './expression_converter';
 import {CompileElement, CompileNode} from './compile_element';
 import {CompileMethod} from './compile_method';
+import {CompilePipe} from './compile_pipe';
 import {ViewType} from 'angular2/src/core/linker/view_type';
 import {
   CompileDirectiveMetadata,
@@ -20,16 +19,11 @@ import {
   getViewFactoryName,
   injectFromViewParentInjector,
   createDiTokenExpression,
-  getPropertyInView
+  getPropertyInView,
+  createPureProxy
 } from './util';
 import {CompilerConfig} from '../config';
 import {CompileBinding} from './compile_binding';
-
-import {bindPipeDestroyLifecycleCallbacks} from './lifecycle_binder';
-
-export class CompilePipe {
-  constructor() {}
-}
 
 export class CompileView implements NameResolver {
   public viewType: ViewType;
@@ -60,7 +54,8 @@ export class CompileView implements NameResolver {
   public subscriptions: o.Expression[] = [];
 
   public componentView: CompileView;
-  public pipes = new Map<string, o.Expression>();
+  public purePipes = new Map<string, CompilePipe>();
+  public pipes: CompilePipe[] = [];
   public variables = new Map<string, o.Expression>();
   public className: string;
   public classType: o.Type;
@@ -68,6 +63,7 @@ export class CompileView implements NameResolver {
 
   public literalArrayCount = 0;
   public literalMapCount = 0;
+  public pipeCount = 0;
 
   constructor(public component: CompileDirectiveMetadata, public genConfig: CompilerConfig,
               public pipeMetas: CompilePipeMetadata[], public styles: o.Expression,
@@ -124,39 +120,17 @@ export class CompileView implements NameResolver {
     }
   }
 
-  createPipe(name: string): o.Expression {
-    var pipeMeta: CompilePipeMetadata = null;
-    for (var i = this.pipeMetas.length - 1; i >= 0; i--) {
-      var localPipeMeta = this.pipeMetas[i];
-      if (localPipeMeta.name == name) {
-        pipeMeta = localPipeMeta;
-        break;
+  callPipe(name: string, input: o.Expression, args: o.Expression[]): o.Expression {
+    var compView = this.componentView;
+    var pipe = compView.purePipes.get(name);
+    if (isBlank(pipe)) {
+      pipe = new CompilePipe(compView, name);
+      if (pipe.pure) {
+        compView.purePipes.set(name, pipe);
       }
+      compView.pipes.push(pipe);
     }
-    if (isBlank(pipeMeta)) {
-      throw new BaseException(
-          `Illegal state: Could not find pipe ${name} although the parser should have detected this error!`);
-    }
-    var pipeFieldName = pipeMeta.pure ? `_pipe_${name}` : `_pipe_${name}_${this.pipes.size}`;
-    var pipeExpr = this.pipes.get(pipeFieldName);
-    if (isBlank(pipeExpr)) {
-      var deps = pipeMeta.type.diDeps.map((diDep) => {
-        if (diDep.token.equalsTo(identifierToken(Identifiers.ChangeDetectorRef))) {
-          return o.THIS_EXPR.prop('ref');
-        }
-        return injectFromViewParentInjector(diDep.token, false);
-      });
-      this.fields.push(
-          new o.ClassField(pipeFieldName, o.importType(pipeMeta.type), [o.StmtModifier.Private]));
-      this.createMethod.resetDebugInfo(null, null);
-      this.createMethod.addStmt(o.THIS_EXPR.prop(pipeFieldName)
-                                    .set(o.importExpr(pipeMeta.type).instantiate(deps))
-                                    .toStmt());
-      pipeExpr = o.THIS_EXPR.prop(pipeFieldName);
-      this.pipes.set(pipeFieldName, pipeExpr);
-      bindPipeDestroyLifecycleCallbacks(pipeMeta, pipeExpr, this);
-    }
-    return pipeExpr;
+    return pipe.call(this, [input].concat(args));
   }
 
   getVariable(name: string): o.Expression {
@@ -165,29 +139,49 @@ export class CompileView implements NameResolver {
     }
     var currView: CompileView = this;
     var result = currView.variables.get(name);
-    var viewPath = [];
     while (isBlank(result) && isPresent(currView.declarationElement.view)) {
       currView = currView.declarationElement.view;
       result = currView.variables.get(name);
-      viewPath.push(currView);
     }
     if (isPresent(result)) {
-      return getPropertyInView(result, viewPath);
+      return getPropertyInView(result, this, currView);
     } else {
       return null;
     }
   }
 
   createLiteralArray(values: o.Expression[]): o.Expression {
-    return o.THIS_EXPR.callMethod('literalArray',
-                                  [o.literal(this.literalArrayCount++), o.literalArr(values)]);
+    var proxyExpr = o.THIS_EXPR.prop(`_arr_${this.literalArrayCount++}`);
+    var proxyParams: o.FnParam[] = [];
+    var proxyReturnEntries: o.Expression[] = [];
+    for (var i = 0; i < values.length; i++) {
+      var paramName = `p${i}`;
+      proxyParams.push(new o.FnParam(paramName));
+      proxyReturnEntries.push(o.variable(paramName));
+    }
+    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalArr(proxyReturnEntries))]),
+                    values.length, proxyExpr, this);
+    return proxyExpr.callFn(values);
   }
-  createLiteralMap(values: Array<Array<string | o.Expression>>): o.Expression {
-    return o.THIS_EXPR.callMethod('literalMap',
-                                  [o.literal(this.literalMapCount++), o.literalMap(values)]);
+
+  createLiteralMap(entries: Array<Array<string | o.Expression>>): o.Expression {
+    var proxyExpr = o.THIS_EXPR.prop(`_map_${this.literalMapCount++}`);
+    var proxyParams: o.FnParam[] = [];
+    var proxyReturnEntries: Array<Array<string | o.Expression>> = [];
+    var values: o.Expression[] = [];
+    for (var i = 0; i < entries.length; i++) {
+      var paramName = `p${i}`;
+      proxyParams.push(new o.FnParam(paramName));
+      proxyReturnEntries.push([entries[i][0], o.variable(paramName)]);
+      values.push(<o.Expression>entries[i][1]);
+    }
+    createPureProxy(o.fn(proxyParams, [new o.ReturnStatement(o.literalMap(proxyReturnEntries))]),
+                    entries.length, proxyExpr, this);
+    return proxyExpr.callFn(values);
   }
 
   afterNodes() {
+    this.pipes.forEach((pipe) => pipe.create());
     this.viewQueries.values().forEach(
         (queries) => queries.forEach((query) => query.afterChildren(this.updateViewQueriesMethod)));
   }

modules/angular2/test/core/linker/change_detection_integration_spec.ts
@@ -460,6 +460,13 @@ export function main() {
            }));
 
         it('should associate pipes right-to-left', fakeAsync(() => {
+             var ctx = _bindSimpleValue('name | multiArgPipe:"a":"b" | multiArgPipe:0:1', Person);
+             ctx.componentInstance.name = 'value';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['value a b default 0 1 default']);
+           }));
+
+        it('should support calling pure pipes with different number of arguments', fakeAsync(() => {
              var ctx = _bindSimpleValue('name | multiArgPipe:"a":"b" | multiArgPipe:0:1:2', Person);
              ctx.componentInstance.name = 'value';
              ctx.detectChanges(false);
@@ -490,6 +497,56 @@ export function main() {
              ctx.detectChanges(false);
 
              expect(renderLog.log).toEqual(['someProp=Megatron']);
+           }));
+
+        it('should call pure pipes only if the arguments change', fakeAsync(() => {
+             var ctx = _bindSimpleValue('name | countingPipe', Person);
+             // change from undefined -> null
+             ctx.componentInstance.name = null;
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0']);
+
+             // change from null -> some value
+             ctx.componentInstance.name = 'bob';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0', 'bob state:1']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['null state:0', 'bob state:1']);
+
+             // change from some value -> some other value
+             ctx.componentInstance.name = 'bart';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues)
+                 .toEqual(['null state:0', 'bob state:1', 'bart state:2']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues)
+                 .toEqual(['null state:0', 'bob state:1', 'bart state:2']);
+
+           }));
+
+        it('should call pure pipes that are used multiple times only when the arguments change',
+           fakeAsync(() => {
+             var ctx = createCompFixture(`<div [someProp]="name | countingPipe"></div><div [someProp]="age | countingPipe"></div>`, Person);
+             ctx.componentInstance.name = 'a';
+             ctx.componentInstance.age = 10;
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1']);
+             ctx.componentInstance.age = 11;
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1', '11 state:2']);
+           }));
+
+        it('should call impure pipes on each change detection run', fakeAsync(() => {
+             var ctx = _bindSimpleValue('name | countingImpurePipe', Person);
+             ctx.componentInstance.name = 'bob';
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['bob state:0']);
+             ctx.detectChanges(false);
+             expect(renderLog.loggedValues).toEqual(['bob state:0', 'bob state:1']);
            }));
       });
 
@@ -1014,6 +1071,7 @@ const ALL_DIRECTIVES = CONST_EXPR([
 
 const ALL_PIPES = CONST_EXPR([
   forwardRef(() => CountingPipe),
+  forwardRef(() => CountingImpurePipe),
   forwardRef(() => MultiArgPipe),
   forwardRef(() => PipeWithOnDestroy),
   forwardRef(() => IdentityPipe),
@@ -1086,7 +1144,13 @@ class DirectiveLog {
 @Pipe({name: 'countingPipe'})
 class CountingPipe implements PipeTransform {
   state: number = 0;
-  transform(value, args = null) { return `${value} state:${this.state ++}`; }
+  transform(value) { return `${value} state:${this.state ++}`; }
+}
+
+@Pipe({name: 'countingImpurePipe', pure: false})
+class CountingImpurePipe implements PipeTransform {
+  state: number = 0;
+  transform(value) { return `${value} state:${this.state ++}`; }
 }
 
 @Pipe({name: 'pipeWithOnDestroy'})
@@ -1095,27 +1159,22 @@ class PipeWithOnDestroy implements PipeTransform, OnDestroy {
 
   ngOnDestroy() { this.directiveLog.add('pipeWithOnDestroy', 'ngOnDestroy'); }
 
-  transform(value, args = null) { return null; }
+  transform(value) { return null; }
 }
 
 @Pipe({name: 'identityPipe'})
 class IdentityPipe implements PipeTransform {
-  transform(value, args = null) { return value; }
+  transform(value) { return value; }
 }
 
 @Pipe({name: 'wrappedPipe'})
 class WrappedPipe implements PipeTransform {
-  transform(value, args = null) { return WrappedValue.wrap(value); }
+  transform(value) { return WrappedValue.wrap(value); }
 }
 
 @Pipe({name: 'multiArgPipe'})
 class MultiArgPipe implements PipeTransform {
-  transform(value, args = null) {
-    var arg1 = args[0];
-    var arg2 = args[1];
-    var arg3 = args.length > 2 ? args[2] : 'default';
-    return `${value} ${arg1} ${arg2} ${arg3}`;
-  }
+  transform(value, arg1, arg2, arg3 = 'default') { return `${value} ${arg1} ${arg2} ${arg3}`; }
 }
 
 @Component({selector: 'test-cmp', template: '', directives: ALL_DIRECTIVES, pipes: ALL_PIPES})

modules/angular2/test/core/linker/view_injector_integration_spec.ts
@@ -232,38 +232,38 @@ class PushComponentNeedsChangeDetectorRef {
 }
 
 @Pipe({name: 'purePipe', pure: true})
-class PurePipe {
+class PurePipe implements PipeTransform {
   constructor() {}
-  transform(value: any, args: any[] = null): any { return this; }
+  transform(value: any): any { return this; }
 }
 
 @Pipe({name: 'impurePipe', pure: false})
-class ImpurePipe {
+class ImpurePipe implements PipeTransform {
   constructor() {}
-  transform(value: any, args: any[] = null): any { return this; }
+  transform(value: any): any { return this; }
 }
 
 @Pipe({name: 'pipeNeedsChangeDetectorRef'})
 class PipeNeedsChangeDetectorRef {
   constructor(public changeDetectorRef: ChangeDetectorRef) {}
-  transform(value: any, args: any[] = null): any { return this; }
+  transform(value: any): any { return this; }
 }
 
 @Pipe({name: 'pipeNeedsService'})
 export class PipeNeedsService implements PipeTransform {
   service: any;
   constructor(@Inject("service") service) { this.service = service; }
-  transform(value: any, args: any[] = null): any { return this; }
+  transform(value: any): any { return this; }
 }
 
 @Pipe({name: 'duplicatePipe'})
 export class DuplicatePipe1 implements PipeTransform {
-  transform(value: any, args: any[] = null): any { return this; }
+  transform(value: any): any { return this; }
 }
 
 @Pipe({name: 'duplicatePipe'})
 export class DuplicatePipe2 implements PipeTransform {
-  transform(value: any, args: any[] = null): any { return this; }
+  transform(value: any): any { return this; }
 }
 
 @Component({selector: 'root'})
@@ -654,7 +654,7 @@ export function main() {
 
       it('should cache pure pipes', fakeAsync(() => {
            var el = createComp(
-               '<div [simpleDirective]="true | purePipe"></div><div [simpleDirective]="true | purePipe"></div>',
+               '<div [simpleDirective]="true | purePipe"></div><div *ngIf="true" [simpleDirective]="true | purePipe"></div>',
                tcb);
            var purePipe1 = el.children[0].inject(SimpleDirective).value;
            var purePipe2 = el.children[1].inject(SimpleDirective).value;

modules/angular2/src/common/pipes/async_pipe.ts
@@ -1,13 +1,6 @@
 import {isBlank, isPresent, isPromise, CONST} from 'angular2/src/facade/lang';
 import {ObservableWrapper, Observable, EventEmitter} from 'angular2/src/facade/async';
-import {
-  Pipe,
-  Injectable,
-  ChangeDetectorRef,
-  OnDestroy,
-  PipeTransform,
-  WrappedValue
-} from 'angular2/core';
+import {Pipe, Injectable, ChangeDetectorRef, OnDestroy, WrappedValue} from 'angular2/core';
 
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
@@ -55,7 +48,7 @@ var __unused: Promise<any>;  // avoid unused import when Promise union types are
  */
 @Pipe({name: 'async', pure: false})
 @Injectable()
-export class AsyncPipe implements PipeTransform, OnDestroy {
+export class AsyncPipe implements OnDestroy {
   /** @internal */
   _latestValue: Object = null;
   /** @internal */
@@ -76,7 +69,7 @@ export class AsyncPipe implements PipeTransform, OnDestroy {
     }
   }
 
-  transform(obj: Observable<any>| Promise<any>| EventEmitter<any>, args?: any[]): any {
+  transform(obj: Observable<any>| Promise<any>| EventEmitter<any>): any {
     if (isBlank(this._obj)) {
       if (isPresent(obj)) {
         this._subscribe(obj);

modules/angular2/src/common/pipes/date_pipe.ts
@@ -101,14 +101,13 @@ export class DatePipe implements PipeTransform {
   };
 
 
-  transform(value: any, args: any[]): string {
+  transform(value: any, pattern: string = 'mediumDate'): string {
     if (isBlank(value)) return null;
 
     if (!this.supports(value)) {
       throw new InvalidPipeArgumentException(DatePipe, value);
     }
 
-    var pattern: string = isPresent(args) && args.length > 0 ? args[0] : 'mediumDate';
     if (isNumber(value)) {
       value = DateWrapper.fromMillis(value);
     }

commit e0220c00d38e1fb7d75ba7e6fdc5227961e91682

fix(compiler): fix where pipes live
Impure pipes need to live on the view
that used them and need a new instance for
each call site.

Impure pipes need to live on the component view, cached across all child views,
and need a new pure proxy for each for
each call site that lives on the view
of the call site.

Fixes #8408

This bug was introduced not long ago by 3978b16231f8c9da53c8364ab3d574c900b7a3d2

modules/@angular/compiler/src/view_compiler/compile_pipe.ts
@@ -7,17 +7,35 @@ import {Identifiers, identifierToken} from '../identifiers';
 import {injectFromViewParentInjector, createPureProxy, getPropertyInView} from './util';
 
 class _PurePipeProxy {
-  constructor(public instance: o.ReadPropExpr, public argCount: number) {}
+  constructor(public view: CompileView, public instance: o.ReadPropExpr, public argCount: number) {}
 }
 
 export class CompilePipe {
-  meta: CompilePipeMetadata;
+  static call(view: CompileView, name: string, args: o.Expression[]): o.Expression {
+    var compView = view.componentView;
+    var meta = _findPipeMeta(compView, name);
+    var pipe: CompilePipe;
+    if (meta.pure) {
+      // pure pipes live on the component view
+      pipe = compView.purePipes.get(name);
+      if (isBlank(pipe)) {
+        pipe = new CompilePipe(compView, meta);
+        compView.purePipes.set(name, pipe);
+        compView.pipes.push(pipe);
+      }
+    } else {
+      // Non pure pipes live on the view that called it
+      pipe = new CompilePipe(view, meta);
+      view.pipes.push(pipe);
+    }
+    return pipe._call(view, args);
+  }
+
   instance: o.ReadPropExpr;
   private _purePipeProxies: _PurePipeProxy[] = [];
 
-  constructor(public view: CompileView, name: string) {
-    this.meta = _findPipeMeta(view, name);
-    this.instance = o.THIS_EXPR.prop(`_pipe_${name}_${view.pipeCount++}`);
+  constructor(public view: CompileView, public meta: CompilePipeMetadata) {
+    this.instance = o.THIS_EXPR.prop(`_pipe_${meta.name}_${view.pipeCount++}`);
   }
 
   get pure(): boolean { return this.meta.pure; }
@@ -35,28 +53,32 @@ export class CompilePipe {
                                        .set(o.importExpr(this.meta.type).instantiate(deps))
                                        .toStmt());
     this._purePipeProxies.forEach((purePipeProxy) => {
-      createPureProxy(
-          this.instance.prop('transform').callMethod(o.BuiltinMethod.bind, [this.instance]),
-          purePipeProxy.argCount, purePipeProxy.instance, this.view);
+      var pipeInstanceSeenFromPureProxy =
+          getPropertyInView(this.instance, purePipeProxy.view, this.view);
+      createPureProxy(pipeInstanceSeenFromPureProxy.prop('transform')
+                          .callMethod(o.BuiltinMethod.bind, [pipeInstanceSeenFromPureProxy]),
+                      purePipeProxy.argCount, purePipeProxy.instance, purePipeProxy.view);
     });
   }
 
-  call(callingView: CompileView, args: o.Expression[]): o.Expression {
+  private _call(callingView: CompileView, args: o.Expression[]): o.Expression {
     if (this.meta.pure) {
+      // PurePipeProxies live on the view that called them.
       var purePipeProxy = new _PurePipeProxy(
-          o.THIS_EXPR.prop(`${this.instance.name}_${this._purePipeProxies.length}`), args.length);
+          callingView, o.THIS_EXPR.prop(`${this.instance.name}_${this._purePipeProxies.length}`),
+          args.length);
       this._purePipeProxies.push(purePipeProxy);
-      return getPropertyInView(
-                 o.importExpr(Identifiers.castByValue)
-                     .callFn([purePipeProxy.instance, this.instance.prop('transform')]),
-                 callingView, this.view)
+      return o.importExpr(Identifiers.castByValue)
+          .callFn([
+            purePipeProxy.instance,
+            getPropertyInView(this.instance.prop('transform'), callingView, this.view)
+          ])
           .callFn(args);
     } else {
       return getPropertyInView(this.instance, callingView, this.view).callMethod('transform', args);
     }
   }
 }
-
 
 function _findPipeMeta(view: CompileView, name: string): CompilePipeMetadata {
   var pipeMeta: CompilePipeMetadata = null;

modules/@angular/compiler/src/view_compiler/compile_view.ts
@@ -127,16 +127,7 @@ export class CompileView implements NameResolver {
   }
 
   callPipe(name: string, input: o.Expression, args: o.Expression[]): o.Expression {
-    var compView = this.componentView;
-    var pipe = compView.purePipes.get(name);
-    if (isBlank(pipe)) {
-      pipe = new CompilePipe(compView, name);
-      if (pipe.pure) {
-        compView.purePipes.set(name, pipe);
-      }
-      compView.pipes.push(pipe);
-    }
-    return pipe.call(this, [input].concat(args));
+    return CompilePipe.call(this, name, [input].concat(args));
   }
 
   getLocal(name: string): o.Expression {

modules/@angular/core/test/linker/change_detection_integration_spec.ts
@@ -68,6 +68,7 @@ import {
   AfterViewInit,
   AfterViewChecked
 } from '@angular/core';
+import {NgFor} from '@angular/common';
 import {By} from '@angular/platform-browser/src/dom/debug/by';
 import {AsyncPipe} from '@angular/common';
 
@@ -544,16 +545,23 @@ export function main() {
 
         it('should call pure pipes that are used multiple times only when the arguments change',
            fakeAsync(() => {
-             var ctx = createCompFixture(`<div [someProp]="name | countingPipe"></div><div [someProp]="age | countingPipe"></div>`, Person);
+             var ctx = createCompFixture(
+                 `<div [someProp]="name | countingPipe"></div><div [someProp]="age | countingPipe"></div>` +
+                     '<div *ngFor="let x of [1,2]" [someProp]="address.city | countingPipe"></div>',
+                 Person);
              ctx.componentInstance.name = 'a';
              ctx.componentInstance.age = 10;
+             ctx.componentInstance.address = new Address('mtv');
              ctx.detectChanges(false);
-             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1']);
+             expect(renderLog.loggedValues)
+                 .toEqual(['mtv state:0', 'mtv state:1', 'a state:2', '10 state:3']);
              ctx.detectChanges(false);
-             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1']);
+             expect(renderLog.loggedValues)
+                 .toEqual(['mtv state:0', 'mtv state:1', 'a state:2', '10 state:3']);
              ctx.componentInstance.age = 11;
              ctx.detectChanges(false);
-             expect(renderLog.loggedValues).toEqual(['a state:0', '10 state:1', '11 state:2']);
+             expect(renderLog.loggedValues)
+                 .toEqual(['mtv state:0', 'mtv state:1', 'a state:2', '10 state:3', '11 state:4']);
            }));
 
         it('should call impure pipes on each change detection run', fakeAsync(() => {
@@ -1098,6 +1106,7 @@ const ALL_DIRECTIVES = /*@ts2dart_const*/[
   forwardRef(() => OrderCheckDirective2),
   forwardRef(() => OrderCheckDirective0),
   forwardRef(() => OrderCheckDirective1),
+  NgFor
 ];
 
 const ALL_PIPES = /*@ts2dart_const*/[

modules/@angular/core/test/linker/view_injector_integration_spec.ts
@@ -37,7 +37,7 @@ import {
   Host,
   SkipSelfMetadata
 } from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgIf, NgFor} from '@angular/common';
 import {getDOM} from '@angular/platform-browser/src/dom/dom_adapter';
 
 const ALL_DIRECTIVES = /*@ts2dart_const*/[
@@ -71,7 +71,8 @@ const ALL_DIRECTIVES = /*@ts2dart_const*/[
   forwardRef(() => DirectiveNeedsChangeDetectorRef),
   forwardRef(() => PushComponentNeedsChangeDetectorRef),
   forwardRef(() => NeedsHostAppService),
-  NgIf
+  NgIf,
+  NgFor
 ];
 
 const ALL_PIPES = /*@ts2dart_const*/[
@@ -670,23 +671,35 @@ export function main() {
 
       it('should cache pure pipes', fakeAsync(() => {
            var el = createComp(
-               '<div [simpleDirective]="true | purePipe"></div><div *ngIf="true" [simpleDirective]="true | purePipe"></div>',
+               '<div [simpleDirective]="true | purePipe"></div><div [simpleDirective]="true | purePipe"></div>' +
+                   '<div *ngFor="let x of [1,2]" [simpleDirective]="true | purePipe"></div>',
                tcb);
            var purePipe1 = el.children[0].inject(SimpleDirective).value;
            var purePipe2 = el.children[1].inject(SimpleDirective).value;
+           var purePipe3 = el.children[2].inject(SimpleDirective).value;
+           var purePipe4 = el.children[3].inject(SimpleDirective).value;
            expect(purePipe1).toBeAnInstanceOf(PurePipe);
-           expect(purePipe1).toBe(purePipe2);
+           expect(purePipe2).toBe(purePipe1);
+           expect(purePipe3).toBe(purePipe1);
+           expect(purePipe4).toBe(purePipe1);
          }));
 
-      it('should not cache pure pipes', fakeAsync(() => {
+      it('should not cache impure pipes', fakeAsync(() => {
            var el = createComp(
-               '<div [simpleDirective]="true | impurePipe"></div><div [simpleDirective]="true | impurePipe"></div>',
+               '<div [simpleDirective]="true | impurePipe"></div><div [simpleDirective]="true | impurePipe"></div>' +
+                   '<div *ngFor="let x of [1,2]" [simpleDirective]="true | impurePipe"></div>',
                tcb);
            var purePipe1 = el.children[0].inject(SimpleDirective).value;
            var purePipe2 = el.children[1].inject(SimpleDirective).value;
+           var purePipe3 = el.children[2].inject(SimpleDirective).value;
+           var purePipe4 = el.children[3].inject(SimpleDirective).value;
            expect(purePipe1).toBeAnInstanceOf(ImpurePipe);
            expect(purePipe2).toBeAnInstanceOf(ImpurePipe);
-           expect(purePipe1).not.toBe(purePipe2);
+           expect(purePipe2).not.toBe(purePipe1);
+           expect(purePipe3).toBeAnInstanceOf(ImpurePipe);
+           expect(purePipe3).not.toBe(purePipe1);
+           expect(purePipe4).toBeAnInstanceOf(ImpurePipe);
+           expect(purePipe4).not.toBe(purePipe1);
          }));
     });
   });